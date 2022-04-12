4ebae7b7c300b87a2c42be3fe0a00c075f080437
Mahathi <mathreya24@amherst.edu>
2022-04-12 18:56:46 -0400

    basic logic down?

--- src/SubstitutionCipher.java
+++ src/SubstitutionCipher.java
@@ -1,19 +1,45 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class SubstitutionCipher extends Cipher {
-    //private long key;
+    List<List<Character>> substKey;
+    Random rand;
 
     public SubstitutionCipher(long k) {
         super(k);
-    }
+        rand = new Random(k);
+        substKey.add(new ArrayList<Character>());
+        substKey.get(0).set(0, (char)0);
+        substKey.get(0).set(1, (char)(rand.nextInt()*255));
+        for (int i = 1; i < 255; i++) {
+            substKey.add(new ArrayList<Character>());
+            substKey.get(i).set(0, (char)i);
+            char c = (char)(rand.nextInt()*255);
+            for (int j = i - 1; j > 0; j--) {
+                if (substKey.get(j).get(1) == c) {
+                    c = (char) (rand.nextInt() * 255);
+                    j = i;
+                }
+            }
+            substKey.get(i).set(1, c);
+            }
+        }
+        //first element is normal, second element is the code
+
+
 
     public List<Character> encrypt(List<Character> cleartext) {
-        List<Character> encryptedText = new ArrayList<>(cleartext.size());
+        List<Character> encryptedText = new LinkedList<Character>();
         for (int i = 0; i < cleartext.size(); i++) {
-            char c = cleartext.get(i);
-            //do something to c
-            encryptedText.set(i, c);
+            for (int j = 0; j < 255; j++) {
+                if (cleartext.get(i) == substKey.get(j).get(0)) {
+                    encryptedText.set(i, substKey.get(j).get(1));
+                    break;
+                }
+
+            }
         }
 
         return encryptedText;
@@ -23,13 +49,18 @@ public class SubstitutionCipher extends Cipher {
     public List<Character> decrypt(List<Character> ciphertext) {
         List<Character> decryptedText = new ArrayList<>(ciphertext.size());
         for (int i = 0; i < ciphertext.size(); i++) {
-            char c = ciphertext.get(i);
-            //do something to c
-            decryptedText.set(i, c);
+            for (int j = 0; j < 255; j++) {
+                if (ciphertext.get(i) == substKey.get(j).get(1)) {
+                    decryptedText.set(i, substKey.get(j).get(0));
+                    break;
+                }
+            }
         }
 
         return decryptedText;
     }
+
+    //
 
 
 }
